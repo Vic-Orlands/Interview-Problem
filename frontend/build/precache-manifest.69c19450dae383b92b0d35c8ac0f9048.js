7540b8e8eae59f3edbfec67908ef684dce2cdf4f
Vic.Orlands <chimezieinnocent39@gmail.com>
2021-05-24 22:29:46 +0100

    Fixed git tracking issues

---
+++ frontend/build/precache-manifest.69c19450dae383b92b0d35c8ac0f9048.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8158d6e9e9b40aae0ad36a1c7cfa617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463ab9fd9fe32ecff0a1",
+    "url": "/static/css/2.de6ad2a8.chunk.css"
+  },
+  {
+    "revision": "208b47697181b6684a7c",
+    "url": "/static/css/main.0846f4ea.chunk.css"
+  },
+  {
+    "revision": "463ab9fd9fe32ecff0a1",
+    "url": "/static/js/2.b75f5b1b.chunk.js"
+  },
+  {
+    "revision": "87f93a7b572a4fab3c66bf8c40b579ba",
+    "url": "/static/js/2.b75f5b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208b47697181b6684a7c",
+    "url": "/static/js/main.3e26e43d.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "14dc9a6dfd00458238b8c4101feab55d",
+    "url": "/static/media/createGif.14dc9a6d.gif"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/static/media/home.4b837e5e.jpg"
+  },
+  {
+    "revision": "6834a5cfc6ce80c7b00d6c5067728e6e",
+    "url": "/static/media/loading.6834a5cf.gif"
+  }
+]);
